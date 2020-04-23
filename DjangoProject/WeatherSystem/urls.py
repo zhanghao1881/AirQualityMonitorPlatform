1e7dfa665b28383d9d5b13da1845a593a35d932e
张浩 <532135344@qq.com>
2020-04-23 22:21:58 +0800

    feat 实现简易用户登录访问验证

--- DjangoProject/WeatherSystem/urls.py
+++ DjangoProject/WeatherSystem/urls.py
@@ -5,17 +5,11 @@ from . import views
 app_name = 'WeatherSystem'
 urlpatterns = [
     # ex: /WeatherSystem/
-    path('', views.login, name='login'),
-    path('login', views.login, name='login'),
-    path('logged', views.logged, name='logged'),
+    path('', views.alogin, name='alogin'),
+    path('alogin', views.alogin, name='alogin'),
+    path('alogged', views.alogged, name='alogged'),
+    path('alogout', views.alogout, name='alogout'),
     path('chart', views.air_quality_chart, name='air_quality_chart'),
     path('map', views.air_quality_map, name='air_quality_map'),
     path('sort', views.air_quality_sort, name='air_quality_sort'),
-    path('post_test', views.post_test, name='post_test'),
-    # # ex: /WeatherSystem/5/
-    # path('<city_name>/', views.detail, name='detail'),
-    # # ex: /WeatherSystem/5/results/
-    # path('<int:city_name>/results/', views.results, name='results'),
-    # # ex: /WeatherSystem/5/vote/
-    # path('<int:city_name>/vote/', views.vote, name='vote'),
 ]
--- DjangoProject/WeatherSystem/views.py
+++ DjangoProject/WeatherSystem/views.py
@@ -4,74 +4,61 @@ from django.template import loader
 from WeatherSystem.models import City,CitySort
 from django.http import HttpResponseRedirect
 from .forms import SearchForm,CITYCATEGORY
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
-def login(request):
+
+
+def alogin(request):
     if request.POST:
-        usr = request.POST['usr']
-        pwd = request.POST['pwd']
-        if len(usr) != 0 or len(pwd) != 0:
-            test = authenticate(username=usr, password=pwd)
-        if test is not None:
-            return render(request, 'WeatherSystem/logged.html')
+        username = request.POST['usr']
+        password = request.POST['pwd']
+        if len(username) == 0 or len(password) == 0:
+            return render(request, 'WeatherSystem/alogin.html')
 
-    return render(request, 'WeatherSystem/login.html')
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            request.session['username'] = username
 
-def post_test(request):
-    city_id = request.POST['usr']
-    print("post test:", city_id)
-    # sort_list = CitySort.objects.order_by('aqi')
-    # city_list = City.objects.order_by('id')
-    # context = {
-    #     # 'sort_list': sort_list,
-    #     'city_list': city_list,
-    #     'form': SearchForm(),
-    #     'img': '../static/img/1.png',
-    # }
-    return render(request, 'WeatherSystem/login.html')
+            return render(request, 'WeatherSystem/alogged.html')
 
-def logged(request):
-    sort_list = CitySort.objects.order_by('aqi')
-    city_list = City.objects.order_by('id')
-    context = {
-        # 'sort_list': sort_list,
-        'city_list': city_list,
-        'form': SearchForm(),
-        'img': '../static/img/1.png',
-    }
-    return render(request, 'WeatherSystem/logged.html', context)
+    return render(request, 'WeatherSystem/alogin.html')
+
+# def post_test(request):
+#     city_id = request.POST['usr']
+#     print("post test:", city_id)
+#     # sort_list = CitySort.objects.order_by('aqi')
+#     # city_list = City.objects.order_by('id')
+#     # context = {
+#     #     # 'sort_list': sort_list,
+#     #     'city_list': city_list,
+#     #     'form': SearchForm(),
+#     #     'img': '../static/img/1.png',
+#     # }
+#     return render(request, 'WeatherSystem/alogin.html')
+
+
+def alogged(request):
+    if not request.session.get('username'):
+        return render(request, 'WeatherSystem/alogin.html')
+    return render(request, 'WeatherSystem/alogged.html')
+
+def alogout(request):
+    logout(request)
+    return render(request, 'WeatherSystem/alogin.html')
 
 def air_quality_chart(request):
-    sort_list = CitySort.objects.order_by('aqi')
-    city_list = City.objects.order_by('id')
-    context = {
-        # 'sort_list': sort_list,
-        'city_list': city_list,
-        'form': SearchForm(),
-        'img': '../static/img/1.png',
-    }
-    return render(request, 'WeatherSystem/air_quality_chart.html', context)
-
+    if not request.session.get('username'):
+        return render(request, 'WeatherSystem/alogin.html')
+    return render(request, 'WeatherSystem/air_quality_chart.html')
 
 def air_quality_sort(request):
-    sort_list = CitySort.objects.order_by('aqi')
-    city_list = City.objects.order_by('id')
-    context = {
-        # 'sort_list': sort_list,
-        'city_list': city_list,
-        'form': SearchForm(),
-        'img': '../static/img/1.png',
-    }
-    return render(request, 'WeatherSystem/air_quality_sort.html', context)
+    if not request.session.get('username'):
+        return render(request, 'WeatherSystem/alogin.html')
+    return render(request, 'WeatherSystem/air_quality_sort.html')
 
 def air_quality_map(request):
-    sort_list = CitySort.objects.order_by('aqi')
-    city_list = City.objects.order_by('id')
-    context = {
-        # 'sort_list': sort_list,
-        'city_list': city_list,
-        'form': SearchForm(),
-        'img': '../static/img/1.png',
-    }
-    return render(request, 'WeatherSystem/air_quality_map.html', context)
+    if not request.session.get('username'):
+        return render(request, 'WeatherSystem/alogin.html')
+    return render(request, 'WeatherSystem/air_quality_map.html')
